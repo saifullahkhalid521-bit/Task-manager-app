3d64eff278993001c09410e9e53585e0a5708f27
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-05-23 10:47:40 +0530

    Styled task cards

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,6 +1,13 @@
 function TaskCard(props) {
   return (
-    <div>
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "10px",
+        marginTop: "10px",
+        borderRadius: "10px",
+      }}
+    >
       <p>{props.taskName}</p>
     </div>
   );
